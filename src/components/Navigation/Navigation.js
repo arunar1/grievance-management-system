678897c79a330651abd9bb86408ee7c9f332fb59
Arun <arunarofficial00@gmail.com>
2024-07-06 12:55:32 +0530

    feat : added the signup page without conditions

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../Pages/Home/Home'
+import Signup from '../Pages/SignUp/Signup'
 export default function Navigation() {
   return (
    <BrowserRouter>
         <Routes>
-            <Route path='/' element={<Home/>} />
+            <Route path='/' element={<Signup/>} />
         </Routes>
    </BrowserRouter>
   )
--- src/components/Pages/SignUp/Signup.js
+++ src/components/Pages/SignUp/Signup.js
@@ -1,7 +1,77 @@
-import React from 'react'
+import React from 'react';
 
 export default function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <form className="row g-3 w-50">
+        <div className="row">
+          <div className="col-md-6">
+            <label htmlFor="inputFirstname4" className="form-label">
+              First Name
+            </label>
+            <input type="text" className="form-control" id="inputFirstname4" />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="inputLastname4" className="form-label">
+              Last Name
+            </label>
+            <input type="text" className="form-control" id="inputLastname4" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <label htmlFor="inputEmail4" className="form-label">
+              Email
+            </label>
+            <input type="email" className="form-control" id="inputEmail4" />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="inputPassword4" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword4"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <label htmlFor="inputCity" className="form-label">
+              City
+            </label>
+            <input type="text" className="form-control" id="inputCity" />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="inputZip" className="form-label">
+              Zip
+            </label>
+            <input type="text" className="form-control" id="inputZip" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="gridCheck"
+              />
+              <label className="form-check-label" htmlFor="gridCheck">
+                Agree to Terms and Conditions
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary">
+              Sign in
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
