8161ebe4bc6afe3fafa1ed8a13ef7ac25361e1d8
Arun <arunarofficial00@gmail.com>
2024-07-10 23:12:09 +0530

    style : commit

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="d-flex flex-column min-vh-100 login-background">
       <Header />
-      <div className="container-fluid flex-grow-1 d-flex align-items-end p-3 justify-content-end">
+      <div className="container-fluid flex-grow-1 d-flex align-items-end justify-content-end p-3 mb-5 m-md-0">
         <Main />
       </div>
     </div>
