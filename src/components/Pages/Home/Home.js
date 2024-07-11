23af360259ab02047652422e8296d5842b3c524f
Arun <arunarofficial00@gmail.com>
2024-07-11 23:50:19 +0530

    style : added style

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Main from "./Main";
-
+import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="d-flex flex-column min-vh-100 login-background">
       <Header />
       <div className="container-fluid d-flex flex-grow-1 align-items-center align-items-md-end pb-md-4">
-        <div className="row w-100">
+        <div className="row w-100 ps-md-2">
           <div className="col-md-6 d-none d-md-flex flex-column justify-content-center align-items-center rounded-pill main-text text-center text-white">
             <h1>Grievance</h1>
             <p>
@@ -16,6 +16,9 @@ export default function Home() {
               reiciendis tempora ex officia illo laborum vitae soluta repellat,
               obcaecati consequuntur est facere odio.
             </p>
+            <Link to='/signup'>
+              <button className="btn btn-success">Register</button>
+            </Link>
           </div>
           <div className="col-md-6 d-flex justify-content-end">
             <div className="pe-md-4">
