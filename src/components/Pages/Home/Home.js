1121c2fa6e5168d4281b5888726fe4c374fa2498
Arun <arunarofficial00@gmail.com>
2024-07-14 21:57:30 +0530

    style : adjusted the homepage

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -8,18 +8,21 @@ export default function Home() {
       <Header />
       <div className="container-fluid d-flex flex-grow-1 align-items-center align-items-md-end pb-md-4">
         <div className="row w-100 ps-md-2">
-          <div className="col-md-6 d-none d-md-flex flex-column justify-content-center align-items-center rounded-pill main-text text-center text-white">
-            <h1>Grievance</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
-              veritatis corporis vel debitis distinctio voluptatum quo
-              reiciendis tempora ex officia illo laborum vitae soluta repellat,
-              obcaecati consequuntur est facere odio.
-            </p>
-            <Link to='/signup'>
-              <button className="btn btn-success">Register</button>
-            </Link>
+          <div className="col-md-6 d-none d-md-flex flex-column justify-content-center align-items-center  text-center text-white">
+            <div className="rounded-pill m-2 p-5 main-text">
+              <h1>Grievance</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+                veritatis corporis vel debitis distinctio voluptatum quo
+                reiciendis tempora ex officia illo laborum vitae soluta
+                repellat, obcaecati consequuntur est facere odio.
+              </p>
+              <Link to="/signup">
+                <button className="btn btn-success">Register</button>
+              </Link>
+            </div>
           </div>
+
           <div className="col-md-6 d-flex justify-content-end">
             <div className="pe-md-4">
               <Main />
