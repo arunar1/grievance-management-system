4fba8e106ba8c985f2729730ed05dcb5c576b791
Arun <arunarofficial00@gmail.com>
2024-07-15 23:56:16 +0530

    added the animation

--- src/components/Pages/Dashboard/Users/User.js
+++ src/components/Pages/Dashboard/Users/User.js
@@ -55,7 +55,7 @@ export default function User() {
             <div className="col-md-6 bg-white">
               <div className="user-data min-vh-md-100 align-items-center d-flex pt-5 pt-md-0 justify-content-center">
                 <div class="text-secondary pt-5 mt-5 mt-md-0 pt-md-0" style={{ textAlign: "center" }}>
-                  <p class="mb-4">
+                  <p class="mb-4 p-3 p-md-0">
                     Hello! This system enables you to interact with us. You can
                     submit your grievance here, and we will be ready to assist
                     you whenever needed.
--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -6,20 +6,63 @@ export default function Home() {
   return (
     <div className="d-flex flex-column min-vh-100 login-background">
       <Header />
+
+      {/* <div className="row">
+        <div className="col-6 d-none d-lg-block  m-5 p-1 text-white">
+          <p >
+            Welcome to our Grievance Management System. Easily submit and track
+            your grievances with our secure and user-friendly platform. Register
+            now to ensure your concerns are heard and addressed promptly.
+          </p>
+          <Link to="/signup">
+            <button className="btn btn-success">Register</button>
+          </Link>
+        </div>
+      </div> */}
+
       <div className="container-fluid d-flex flex-grow-1 align-items-center align-items-md-end pb-md-4">
         <div className="row w-100 ps-md-2">
           <div className="col-lg-7 d-none d-md-flex flex-column justify-content-center align-items-center  text-center text-white">
-            <div className="rounded-pill m-2 p-5 main-text">
-              <h1>Grievance</h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
-                veritatis corporis vel debitis distinctio voluptatum quo
-                reiciendis tempora ex officia illo laborum vitae soluta
-                repellat, obcaecati consequuntur est facere odio.
-              </p>
-              <Link to="/signup">
-                <button className="btn btn-success">Register</button>
-              </Link>
+            <div className="rounded m-2 p-5 main-text">
+              <div
+                id="carouselExampleAutoplaying"
+                class="carousel slide"
+                data-bs-ride="carousel"
+                data-bs-interval="2000"
+                data-bs-pause="hover"
+              >
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <h3>Simplified Grievance Submission</h3>
+                    <p>
+                      Our Grievance Management System makes it easy to submit
+                      complaints or feedback through a user-friendly interface,
+                      ensuring your voice is heard and addressed promptly.
+                      Suitable for organizations of all sizes, it handles
+                      various types of grievances efficiently.
+                    </p>
+                  </div>
+                  <div className="carousel-item">
+                    <h3>Secure and Confidential</h3>
+                    <p>
+                      We prioritize the security and confidentiality of all
+                      grievances. Robust security measures protect user data,
+                      ensuring complaints are handled discreetly and
+                      professionally, maintaining the integrity of the grievance
+                      process.
+                    </p>
+                  </div>
+                  <div className="carousel-item">
+                    <h3>Efficient Tracking and Resolution</h3>
+                    <p>
+                      Easily track the status of your grievances with real-time
+                      updates from submission to resolution. This transparency
+                      builds trust and ensures accountability, fostering a
+                      positive environment within the organization.
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
 
