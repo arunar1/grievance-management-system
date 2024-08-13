8381026e4c8848177d78661e4050471ab0b37fdd
Arun <arunarofficial00@gmail.com>
2024-08-13 23:21:23 +0530

    add : added pages for supervisor

---
+++ src/components/Pages/Dashboard/Supervisor/GenerateReport.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function GenerateReport() {
+  return (
+    <div>GenerateReport</div>
+  )
+}
---
+++ src/components/Pages/Dashboard/Supervisor/MonitorProgress.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MonitorProgress() {
+  return (
+    <div>MonitorProgress</div>
+  )
+}
