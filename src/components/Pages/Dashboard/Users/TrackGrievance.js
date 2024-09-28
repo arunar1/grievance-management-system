aa2608ebb506adf6decad95ac42cd99ffdbbff10
arun ar <arunarofficial00@gmail.com>
2024-09-28 20:59:23 +0530

    commit

--- src/components/Pages/Dashboard/Users/TrackGrievance.js
+++ src/components/Pages/Dashboard/Users/TrackGrievance.js
@@ -1,36 +1,62 @@
-import React, { useContext } from 'react'
-import { userGrievanceContext } from '../../../Hooks/context/UserGrievance'
+import React, { useContext } from "react";
+import { userGrievanceContext } from "../../../Hooks/context/UserGrievance";
 import { GrievanceCard } from "../../../Card/GrievanceCard";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
 
 export default function TrackGrievance() {
-
   const navigation = useNavigate();
   const { userGrievance } = useContext(userGrievanceContext);
-  console.log(userGrievance)
-  const Card = [...userGrievance].reverse()?.map((item)=>{
-    return <GrievanceCard data={item} />;
-  })
 
+  const unresolvedGrievances = [...userGrievance]
+    .reverse()
+    ?.filter((item) => item.status !== "Resolved")
+    .map((item) => {
+      return <GrievanceCard data={item} key={item.slNumber} />;
+    });
+
+  const resolvedGrievances = [...userGrievance]
+    .reverse()
+    ?.filter((item) => item.status === "Resolved")
+    .map((item) => {
+      return <GrievanceCard data={item} key={item.slNumber} />;
+    });
 
   return (
     <div className="container-fluid">
       <div className="backbtn mt-2 mb-5">
         <i
-          class="btn fa-solid fa-arrow-left fa-2x"
+          className="btn fa-solid fa-arrow-left fa-2x"
           onClick={(e) => {
             e.stopPropagation();
             navigation(-1);
           }}
         ></i>
       </div>
+
       <h2 className="mb-4 container">Track Grievances</h2>
-      {Card}
-      {userGrievance.length == 0 && (
+
+      <div className="unresolved-grievances mb-5 container">
+        <h5 className=" text-primary">Unresolved Grievances</h5>
+        {unresolvedGrievances.length > 0 ? (
+          unresolvedGrievances
+        ) : (
+          <p>No unresolved grievances found.</p>
+        )}
+      </div>
+
+      <div className="resolved-grievances container">
+        <h5 className=" text-primary">Resolved Grievances</h5>
+        {resolvedGrievances.length > 0 ? (
+          resolvedGrievances
+        ) : (
+          <p>No resolved grievances found.</p>
+        )}
+      </div>
+
+      {userGrievance.length === 0 && (
         <div
           className="container text-center d-flex justify-content-center align-items-center"
-          style={{ height: "300px",}}
+          style={{ height: "300px" }}
         >
           <h5>No Grievance Submitted</h5>
         </div>
